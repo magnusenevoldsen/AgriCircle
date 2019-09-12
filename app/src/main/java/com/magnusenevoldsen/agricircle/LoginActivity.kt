528de8e5c6d0d315d9c64f4c00d7cba3e69ba1fe
magnaw <magnus_enevoldsen@live.dk>
2019-09-12 13:09:22 +0200

    Changes to login screen, started working on the correct textfields

--- app/src/main/java/com/magnusenevoldsen/agricircle/LoginActivity.kt
+++ app/src/main/java/com/magnusenevoldsen/agricircle/LoginActivity.kt
@@ -2,11 +2,22 @@ package com.magnusenevoldsen.agricircle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+
     }
 }
