a4d890a173899f045097107be7825b960f63a97f
magnaw <magnus_enevoldsen@live.dk>
2019-10-15 17:23:01 +0200

    Changed it so .svg images are loaded into a webview instead

--- app/src/main/java/com/magnusenevoldsen/agricircle/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/magnusenevoldsen/agricircle/ui/settings/SettingsFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -15,6 +18,8 @@ import com.squareup.picasso.Picasso
 class SettingsFragment : Fragment() {
 
     private lateinit var settingsViewModel: SettingsViewModel
+    var userImageView : ImageView? = null
+    var userWebView : WebView? = null
 
 
     override fun onCreateView(
@@ -28,20 +33,30 @@ class SettingsFragment : Fragment() {
             ViewModelProviders.of(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
 
-        val userImageView : ImageView = root.findViewById(R.id.userImageView)
+        userImageView = root.findViewById(R.id.userImageView)
+        userWebView = root.findViewById(R.id.userWebView)
 
         val userNameTextView : TextView = root.findViewById(R.id.userNameTextView)
 
 
         //Load data into page:
         try {
+            //Find the Url and then find the extension
             var avatarUrl : String = AgriCircleBackend.user!!.avatarUrl
             var shortened : String = avatarUrl.substring(avatarUrl.length - 4, avatarUrl.length)
 
-
+            //If its a .svg -> Load it into a webview instead
             if (shortened.equals(".svg")) {
-
+                changeImageView(true) //Use WebView
+                userWebView!!.webViewClient = object : WebViewClient() {
+                    override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                        view?.loadUrl(url)
+                        return true
+                    }
+                }
+                userWebView!!.loadUrl(avatarUrl)
             } else {
+                changeImageView(false) //Use ImageView
                 Picasso.get().load(avatarUrl).into(userImageView)
             }
 
@@ -65,4 +80,38 @@ class SettingsFragment : Fragment() {
 
         return root
     }
+
+
+    fun changeImageView(option : Boolean) {
+        //True = show WebView
+        //False = show ImageView
+        if (option) {
+            userWebView!!.visibility = View.VISIBLE
+            userImageView!!.visibility = View.INVISIBLE
+        }
+        else if (!option) {
+            userWebView!!.visibility = View.INVISIBLE
+            userImageView!!.visibility = View.VISIBLE
+        }
+
+    }
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
